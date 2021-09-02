660a554c4579f749a49dc509deea3d41abcdf6a5
kumar <santanupaikaray23@gmail.com>
2021-09-02 11:00:31 +0530

    add all changes

--- src/Component/Home/Search.js
+++ src/Component/Home/Search.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import './Search.css';
-const serviceUrl="http://localhost:9800/services";
-const servicetypeUrl=""
+const serviceUrl="http://localhost:9800/service";
+const typeUrl="http://localhost:9800/types?service=";
 
 class Search extends Component {
     constructor(){
         super()
         console.log(">>>in constructor")
         this.state={
-            service:''
+            service:'',
+            types:''
         }
     }
-    renderservices = (data)=>{
+    renderservice = (data)=>{
         if(data){
             return data.map((item)=>{
                 return(
@@ -23,9 +24,29 @@ class Search extends Component {
         }
 
         }
-  
+        rendertype = (data)=>{
+            if(data){
+                return data.map((item)=>{
+                    return(
+                        <option value={item._id}>
+                            {item.name}|{item.about}
+                        </option>
+                    )
+                })
+            }
+    
+            }
+  handleService = (event) => {
+      console.log(event.target.value)
+      const serviceId = event.target.value;
+      fetch(`${typeUrl}${serviceId}`,{method:'GET'})
+      .then((res)=>res.json())
+      .then((data)=>{
+          this.setState({types:data})
+      })
+  }
     render(){
-        console.log(">>>>>in render>>>>>",this.state.service)
+        console.log(">>>>>in render>>>>>",this.state.types)
         return(
             <div className="imageContainer">
                 <div id="logo"> 
@@ -38,19 +59,21 @@ class Search extends Component {
                     Nature With Code
 
                 </div>
-                <div className="locationSelector">
-                    <select className="locationDropDown">
+                <div className="serviceSelector">
+                    <select className="serviceDropDown" onChange={this.handleService}>
                         <option>--SELECT SERVICES--</option>
-                        {this.renderservices(this.state.services)}
+                        {this.renderservice(this.state.services)}
                     </select>
                     <select className="typesDropDown">
-                        <option>--SELECT SERVICES TYPE--</option>
+                        <option>-----SELECT TYPES-----</option>
+                        {this.rendertype(this.state.types)}
+
                     </select>
                     </div>
             </div>
         )
     }
-    ///call api
+    ///on page load
     componentDidMount(){
         console.log(">>>in componentDidMount")
         //We get the data and update the state
